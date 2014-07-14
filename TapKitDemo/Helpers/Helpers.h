6839b70b5049c231de94f64ea89b9546e39c5dc3
Kevin Wu <kvmisc@163.com>
2014-07-14 10:18:20 +0800

    blah blah

---
+++ TapKitDemo/Helpers/Helpers.h
@@ -0,0 +1,9 @@
+//
+//  Helpers.h
+//  TapKitDemo
+//
+//  Created by Kevin on 7/14/14.
+//  Copyright (c) 2014 Tapmob. All rights reserved.
+//
+
+
--- TapKitDemo/Sections/NSData+RSA.m
+++ TapKitDemo/Sections/NSData+RSA.m
@@ -2,7 +2,7 @@
 //  NSData+RSA.m
 //  TapKitDemo
 //
-//  Created by Kevin Wu on 7/11/14.
+//  Created by Kevin on 7/11/14.
 //  Copyright (c) 2014 Tapmob. All rights reserved.
 //
 
